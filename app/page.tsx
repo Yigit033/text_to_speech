3cb0390d17fa33b8ad2dba2674faee4abd5aab7f
Yiğit Tilaver <yigittilaver2000@gmail.com>
2024-01-25 13:27:26 +0300

    added UserButton

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
+import { UserButton } from "@clerk/nextjs";
 import GenerateSoundView from "./views/GenerateSoundView";
 
 /**
  * The main entry point component for the application.
  * It renders the GenerateSoundView component.
  */
-export default function Home() {
+
+export default function Home({ className }: UserProps) {
   return (
     <main className="flex min-h-screen flex-col">
       {/* Render the GenerateSoundView component */}
+      <div className="flex justify-end items-center p-4  size-15">
+        <UserButton afterSignOutUrl="/" />
+      </div>
       <GenerateSoundView />
     </main>
   );
